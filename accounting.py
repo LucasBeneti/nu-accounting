437e756f3465b35e1a87211ecf9afc36913aff19
Lucas <lucas.beneti@akiyama.com.br>
2020-02-27 01:09:09 -0300

    Tested the API at version 1.0.1 and it works. Currently the answer of the get_bills() function it's being written on a pickle file for further use

--- accounting.py
+++ accounting.py
@@ -1,11 +1,26 @@
+
+# from pynubank import Nubank
+
+# user = os.getenv('NU_USER')
+# passwd = os.getenv('NU_PASSWD')
+
+# nu = Nubank()
+# nu.authenticate(user, passwd)
+
+# bills = nu.get_bills()
+# print(len(bills))
 import os
 from pynubank import Nubank
 
 user = os.getenv('NU_USER')
 passwd = os.getenv('NU_PASSWD')
-
 nu = Nubank()
-nu.authenticate(user, passwd)
-
-bills = nu.get_bills()
-print(len(bills))
+uuid, qr_code = nu.get_qr_code()
+# Nesse momeento será printado o QRCode no console
+# Você precisa escanear pelo o seu app do celular
+# Esse menu fica em NU > Perfil > Acesso pelo site
+qr_code.print_ascii(invert=True)
+input('Após escanear o QRCode pressione enter para continuar')
+# Somente após escanear o QRCode você pode chamar a linha abaixo
+nu.authenticate_with_qr_code(user, passwd, uuid)
+print(nu.get_bills())
